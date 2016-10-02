3e26f84c5a95ae510639d8b06a070724f7957528
timmymarion <timothy.marion@marion.systems>
2016-10-02 16:01:52 -0400

    complete midterm

--- midterm.py
+++ midterm.py
@@ -122,3 +122,15 @@ def g(i):
 L = [2, 1, 0, -1, -2, -3, -4, -5]
 # print(applyF_filterG(L, f, g))
 # print(L)
+
+def flatten(aList):
+    '''
+    aList: a list
+    Returns a copy of aList, which is a flattened version of aList
+    '''
+
+    if aList == []:
+        return aList
+    if isinstance(aList[0], list):
+        return flatten(aList[0]) + flatten(aList[1:])
+    return aList[:1] + flatten(aList[1:])
